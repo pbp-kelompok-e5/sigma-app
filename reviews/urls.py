d7e0dd387f2e16174fe01912cd6f937ebbb98c96
Gerrybp12 <gerry.bimaputra@gmail.com>
2025-10-22 14:39:26 +0700

    add: disable button submit button, star design, reviews page per user

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,5 @@ app_name = 'reviews'
 
 urlpatterns = [
     path('<int:event_id>/', views.event_reviews, name='event-reviews'),
+    path('user/<int:user_id>/', views.user_reviews, name='user-reviews'),
 ]
